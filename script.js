1658438138876fec9ed5b665c3f9f8ad567c318f
Leo Danenkov <ledanenkov@gmail.com>
2024-02-01 20:38:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
             scale: 0.6,
             rotation: "0 0 0", 
             position: "0 0.1 0", 
+            volumePosition: "0 0.1 0",
             desc: "Ingredients: Bean sprouts, Bamboo shoots, Broths (Miso, Shoyu, Shio, or Tonkotsu), Chashu pork, Chicken, Corn, Ginger, Green onions, Miso paste, Mushrooms, Nori, Ramen noodles, Sesame oil, Sesame seeds, Soy sauce, Tofu. Allergens: Eggs (used in soft-boiled eggs), Sesame (in sesame oil and seeds), Soy (in soy sauce and toppings), Wheat (in noodles and broths)."
         },
 
@@ -22,6 +23,7 @@
             scale: 0.09,
             rotation: "0 0 0",
             position: "0 0.1 0",
+            volumePosition: "0 0.1 0",
             desc: "Ingredients: Sashimi-grade fish (such as tuna or salmon), rice (check for potential cross-contamination if allergic to gluten), avocado, cucumber, edamame, seaweed (Nori), soy sauce, pickled ginger, wasabi. Allergens: soy sauce, fish. "
         },
 
@@ -33,6 +35,7 @@
             scale: 0.095,
             rotation: "0 0 0",
             position: "0 0.1 0",
+            volumePosition: "0 0.1 0",
             desc: "Ingredients: Sushi Rice, Salmon (Shake), Avocado, Cucumber, Nori (Seaweed), Sesame Seeds, Soy Sauce (contains soy and wheat; choose gluten-free if needed), Pickled Ginger, Wasabi. Allergens: soy, fish (salmon)."
         },
 
@@ -44,6 +47,7 @@
             scale: 0.2,
             rotation: "0 0 0",
             position: "0 0.1 0",
+            volumePosition: "0 0.1 0",
             desc: "Ingredients: pizza dough, fresh tomatoes, fresh mozzarella cheese, fresh basil, olive oil, garlic, salt and pepper to taste. Allergens: gluten (dough), dairy (cheese)."
         },
 
@@ -55,6 +59,7 @@
             scale: 0.2,
             rotation: "0 0 0",
             position: "0 0.1 0",
+            volumePosition: "0 0.1 0",
             desc: "Ingredients: Pizza dough, fresh tomatoes, mozzarella cheese, bacon, red pepper, black pepper (spice), mushrooms. Allergens: gluten (dough), dairy (cheese)."
         }
     ],
@@ -91,7 +96,7 @@
             }
         });
     }
-    }
+}
 
     //Magic reactivity
 
@@ -114,17 +119,20 @@
         model.dataset.rotation = dish.rotation;
         model.object3D.visible = false; 
 
-        model.onload = () => {
-            model.components.set('model', dish.model)
-        }
+        let volumeModel = document.createElement("mr-model");
+
+        volumeModel.setAttribute("src", "./assets/" + dish.volumeModel);
+        volumeModel.dataset.name = dish.name;
+        volumeModel.dataset.position = dish.volumePosition;
+        volumeModel.object3D.visible = false;
 
         Object.assign(model.style, {
             scale: dish.scale
         })
 
         document.querySelector("#models").append(model);
+        document.querySelector("#food-models").append(volumeModel);
 
-        
     });
 
     document.addEventListener('DOMContentLoaded', function(){
